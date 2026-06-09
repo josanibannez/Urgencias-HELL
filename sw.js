d359fb63f37022245252f3291071c5f544c89909
josanibannez <josan.ibannez@gmail.com>
2026-06-09 22:17:35 +0200

    Forzar actualización caché v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'urgencias-hel-v1';
+const CACHE = 'urgencias-hell-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e =>
